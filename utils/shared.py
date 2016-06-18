228f6a86e9c3a30995bb5c546ca335f41a9f0d65
Stein Eldar Johnsen <morimekta@gmail.com>
2016-06-18 21:30:08 +0200

    Remove old python implementation.

--- utils/shared.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def path(file):
-    return os.path.normpath(os.path.join(__shared, file))
-
-## --- private ---
-
-__sp, __sn = os.path.split(os.path.realpath(__file__))
-__shared = os.path.normpath(os.path.join(__sp, '../share'))
